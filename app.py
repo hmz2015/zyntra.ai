a3be4853f916bd5b1d6a9af89e832732481f9a2e
hmz2015 <elhedadi.hamza@gmail.com>
2026-04-24 17:47:41 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -565,4 +565,4 @@ SLIDE: Conclusion
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-        socketio.run(app, debug=True, port=5000, allow_unsafe_werkzeug=True)
+    socketio.run(app, debug=True, port=5000, allow_unsafe_werkzeug=True)
